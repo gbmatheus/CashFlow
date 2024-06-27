2dea90139f4238534d88b9820230c7c89a1277fd
gbmatheus <gabrielmatheus2503@gmail.com>
2024-06-27 17:16:06 -0300

    fix: changed name attribute for use PascalCase

--- src/CashFlow.Comunication/Requests/RequestRegisterExpenseJson.cs
+++ src/CashFlow.Comunication/Requests/RequestRegisterExpenseJson.cs
@@ -8,7 +8,7 @@ namespace CashFlow.Comunication.Requests
         public string Description { get; set; } = string.Empty;
         public DateTime Date {  get; set; }
         public decimal Amount { get; set; }
-        public PaymentType paymentType { get; set; }
+        public PaymentType PaymentType { get; set; }
 
     }
 }
