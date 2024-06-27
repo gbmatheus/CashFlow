992eb39ef14e9243b6cf64af6c4f5a6c78a7f5b4
gbmatheus <gabrielmatheus2503@gmail.com>
2024-06-27 17:51:14 -0300

    feat: added new contructor with list string into ResponseErrorJson

--- src/CashFlow.Comunication/Responses/ResponseErrorJson.cs
+++ src/CashFlow.Comunication/Responses/ResponseErrorJson.cs
@@ -2,10 +2,16 @@
 {
     public class ResponseErrorJson
     {
-        public string ErrorMessage {  get; set; } = string.Empty;
+        public List<string> ErrorMessages {  get; set; }
 
-        public ResponseErrorJson(string errorMensage) { 
-            ErrorMessage = errorMensage;
+        public ResponseErrorJson(string errorMensage) 
+        { 
+            ErrorMessages = new List<string> { errorMensage };
+        }
+
+        public ResponseErrorJson(List<string> errorMensages)
+        {
+            ErrorMessages = errorMensages;
         }
     }
 }
