390f4edce4e0d38fd34320ec1746abd64d926d32
gbmatheus <gabrielmatheus2503@gmail.com>
2024-06-27 17:17:09 -0300

    feat: added validation with FluentValidation

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -14,28 +14,9 @@ namespace CashFlow.Application.UseCases.Expenses.Register
 
         private void Validate(RequestRegisterExpenseJson request)
         {
-            var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
-            if(titleIsEmpty)
-            {
-                throw new ArgumentException("The title is required");
-            }
+            var validator = new RegisterExpenseValidator();
 
-            if(request.Amount <= 0)
-            {
-                throw new ArgumentException("The Amoun must be greater than zero");
-            }
-            
-            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
-            if (result > 0)
-            {
-                throw new ArgumentException("Expenses cannot be for the future");
-            }
-
-            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.paymentType);
-            if(!paymentTypeIsValid)
-            {
-                throw new ArgumentException("Payment type is not valid");
-            }
+            var result = validator.Validate(request);
         }
 
     }
