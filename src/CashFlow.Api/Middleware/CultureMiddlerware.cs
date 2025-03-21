1e26edf387d1de8eb45d1c5197bfc016fdb774fd
gbmatheus <gabrielmatheus2503@gmail.com>
2025-03-21 18:22:41 -0300

    feat: added config to globalization middleware

---
+++ src/CashFlow.Api/Middleware/CultureMiddlerware.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace CashFlow.Api.Middleware
+{
+    public class CultureMiddlerware
+    {
+        private readonly RequestDelegate _next;
+        private const string CULTURE_INFO_DEFAULT = "en";
+
+        public CultureMiddlerware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke (HttpContext context)
+        {
+            var suportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+
+            var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+            var culture = new CultureInfo(CULTURE_INFO_DEFAULT);
+
+            if(string.IsNullOrWhiteSpace(requestedCulture)
+                && suportedLanguages.Exists(language => language.Name.Equals(requestedCulture))
+            )
+                culture = new CultureInfo(requestedCulture);
+
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
+
+            await _next(context);
+        }
+
+    }
+}
--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -1,4 +1,5 @@
 using CashFlow.Api.Filter;
+using CashFlow.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<CultureMiddlerware>();
 
 app.UseHttpsRedirection();
 
