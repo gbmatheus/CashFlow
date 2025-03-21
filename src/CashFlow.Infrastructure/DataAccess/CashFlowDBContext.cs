400ddc10c972bdfc1a45ee7529b9a09f114ffc0e
gbmatheus <gabrielmatheus2503@gmail.com>
2025-03-21 18:28:11 -0300

    feat: added DB context configuration and packages to perform operations with the database

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDBContext.cs
@@ -0,0 +1,21 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess
+{
+    public class CashFlowDBContext: DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            string connectionServer = "server=127.0.0.1;uid=root;pwd=Senha123;database=cashflow_db";
+            var version = new Version(9, 2, 0);
+            var mySqlSeverVersion = new MySqlServerVersion(version);
+
+            optionsBuilder.UseMySql(connectionServer, mySqlSeverVersion);
+        }
+    }
+}
