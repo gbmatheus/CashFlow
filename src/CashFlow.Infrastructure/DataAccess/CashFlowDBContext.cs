a190bf0c012d90930595b7e7fcf1a75a6a9b82b8
gbmatheus <gabrielmatheus2503@gmail.com>
2025-03-22 15:43:55 -0300

    feat: added injection to db context and repositories

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDBContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDBContext.cs
@@ -3,19 +3,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CashFlow.Infrastructure.DataAccess
 {
-    public class CashFlowDBContext: DbContext
+    internal class CashFlowDBContext : DbContext
     {
         public DbSet<Expense> Expenses { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-
-            string connectionServer = "server=127.0.0.1;uid=root;pwd=Senha123;database=cashflow_db";
-            var version = new Version(9, 2, 0);
-            var mySqlSeverVersion = new MySqlServerVersion(version);
-
-            optionsBuilder.UseMySql(connectionServer, mySqlSeverVersion);
-        }
+        public CashFlowDBContext(DbContextOptions options) : base(options) { }
     }
 }
---
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,32 @@
+﻿using CashFlow.Domain.Repositories;
+using CashFlow.Infrastructure.DataAccess;
+using CashFlow.Infrastructure.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CashFlow.Infrastructure
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddDbContext(services, configuration);
+            AddRepositories(services);
+        }
+
+        private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionServer = configuration.GetConnectionString("Connection");
+            var version = new Version(9, 2, 0);
+            var mySqlSeverVersion = new MySqlServerVersion(version);
+
+            services.AddDbContext<CashFlowDBContext>(config => config.UseMySql(connectionServer, mySqlSeverVersion));
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IExpenseRepository, ExpenseRepository>();
+        }
+    }
+}
