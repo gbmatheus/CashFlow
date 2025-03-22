0ed3734484f85a58204eac7c3b5c1e7ca95e3f25
gbmatheus <gabrielmatheus2503@gmail.com>
2025-03-21 22:19:07 -0300

    feat: added expense repository

---
+++ src/CashFlow.Domain/Repositories/IExpenseRepository.cs
@@ -0,0 +1,9 @@
+﻿using CashFlow.Domain.Entities;
+
+namespace CashFlow.Domain.Repositories
+{
+    public interface IExpenseRepository
+    {
+        void Add(Expense expense);
+    }
+}
---
+++ src/CashFlow.Infrastructure/DataAccess/Repositories/ExpenseRepository.cs
@@ -0,0 +1,17 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories;
+
+namespace CashFlow.Infrastructure.DataAccess.Repositories
+{
+    internal class ExpenseRepository : IExpenseRepository
+    {
+        public void Add(Expense expense)
+        {
+            var dbContext = new CashFlowDBContext();
+
+            dbContext.Expenses.Add(expense);
+
+            dbContext.SaveChanges();
+        }
+    }
+}
