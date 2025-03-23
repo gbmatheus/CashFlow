6d37fcdb4485310b7b34e44f03ee5f0ea2450621
gbmatheus <gabrielmatheus2503@gmail.com>
2025-03-23 13:32:55 -0300

    feat: changed erro on validation constructor

--- src/CashFlow.Exception/ExceptionBase/ErroOnvalidationException.cs
+++ src/CashFlow.Exception/ExceptionBase/ErroOnvalidationException.cs
@@ -1,12 +1,12 @@
 ﻿namespace CashFlow.Exception.ExceptionBase
 {
-    public class ErroOnValidationException: CashFlowException
+    public class ErroOnValidationException : CashFlowException
     {
 
         public List<string> Errors { get; set; }
 
-        public ErroOnValidationException(List<string> errorMessages) 
-        { 
+        public ErroOnValidationException(List<string> errorMessages) : base(string.Empty)
+        {
             Errors = errorMessages;
         }
     }
