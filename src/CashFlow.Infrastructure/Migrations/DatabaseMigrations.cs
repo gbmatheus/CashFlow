4c96d88ccc54465af62bc2de96ad9a6d160fb888
gbmatheus <gabrielmatheus2503@gmail.com>
2025-03-31 17:52:37 -0300

    feat: added database migration and configuration to run the migration through the API

---
+++ src/CashFlow.Infrastructure/Migrations/DatabaseMigrations.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CashFlow.Infrastructure.Migrations
+{
+    public static class DatabaseMigrations
+    {
+        public static async Task MigrateDatabase(IServiceProvider serviceProvider)
+        {
+            var dbContext = serviceProvider.GetRequiredService<CashFlowDBContext>();
+
+            await dbContext.Database.MigrateAsync();
+        }
+    }
+}
