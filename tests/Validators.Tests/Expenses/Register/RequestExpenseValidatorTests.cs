110886b51e71489aca2db92bb7f68ae46e0eb9c7
gbmatheus <gabrielmatheus2503@gmail.com>
2025-05-01 14:13:40 -0300

    test: added sucess  scenario to validator test

---
+++ tests/Validators.Tests/Expenses/Register/RequestExpenseValidatorTests.cs
@@ -0,0 +1,25 @@
+﻿using CashFlow.Application.UseCases.Expenses;
+using CashFlow.Comunication.Enums;
+using CashFlow.Comunication.Requests.Expenses;
+using CommonTestUtilities.Request;
+
+namespace Validators.Tests.Expenses.Register
+{
+    public class RequestExpenseValidatorTests
+    {
+        [Fact]
+        public void Sucess()
+        {
+            // Triple A - Arrange, Act, Assert
+            // Arrange - Cria as instâncias e prepara os dados necessário para fazer a execução do teste
+            RequestExpenseJson request = RequestExpenseJsonBuilder.Build();
+            var validator = new ExpenseValidator();
+
+            // Act - Realiza a operação/situação que será testada
+            var result = validator.Validate(request);
+
+            // Assert - Valida o resultado
+            Assert.True(result.IsValid);
+        }
+    }
+}
