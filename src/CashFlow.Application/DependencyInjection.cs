ef01b27cbd79a5e3b6dd38fdf784cda010caed45
gbmatheus <gabrielmatheus2503@gmail.com>
2025-05-09 18:43:14 -0300

    fix: added dependency injection for resgister use case and map user request

--- src/CashFlow.Application/DependencyInjection.cs
+++ src/CashFlow.Application/DependencyInjection.cs
@@ -4,6 +4,7 @@ using CashFlow.Application.UseCases.Expenses.GetAll;
 using CashFlow.Application.UseCases.Expenses.GetById;
 using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Application.UseCases.Expenses.Update;
+using CashFlow.Application.UseCases.Users.Register;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CashFlow.Application
@@ -23,6 +24,8 @@ namespace CashFlow.Application
             services.AddScoped<IGetByIdExpenseUseCase, GetByIdExpenseUseCase>();
             services.AddScoped<IDeleteExpenseUseCase, DeleteExpenseUseCase>();
             services.AddScoped<IUpdateExpenseUseCase, UpdateExpenseUseCase>();
+
+            services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
         }
 
         private static void AddAutoMapper(IServiceCollection services)
