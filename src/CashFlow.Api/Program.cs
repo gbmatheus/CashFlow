40833c25f985edb5f6de4d65400f37b24e2ecec3
gbmatheus <gabrielmatheus2503@gmail.com>
2025-05-17 11:46:54 -0300

    docs: configure Swagger to support token passing

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -4,9 +4,8 @@ using CashFlow.Application;
 using CashFlow.Infrastructure;
 using CashFlow.Infrastructure.Migrations;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,7 +15,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config => config.AddSecurityDefinition("BearerToken", new OpenApiSecurityScheme
+{
+    Name = "Authorization",
+    Description = "Bearer {Token}",
+    In = ParameterLocation.Header,
+    Scheme = "Bearer",
+    Type = SecuritySchemeType.ApiKey
+}));
+
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -30,11 +37,11 @@ builder.Services.AddAuthentication(options =>
 }).AddJwtBearer(options =>
 {
     options.TokenValidationParameters = new TokenValidationParameters
-{
-    ValidateIssuer = false,
-    ValidateAudience = false,
-    ClockSkew = TimeSpan.Zero,
-    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!))
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = TimeSpan.Zero,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!))
     };
 });
 
