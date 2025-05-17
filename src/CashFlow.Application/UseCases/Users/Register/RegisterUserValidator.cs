21c2622f3b2b8a3a40942ed263c334a922ba2bad
gbmatheus <gabrielmatheus2503@gmail.com>
2025-05-17 13:49:05 -0300

    feat: changed user validator to apply only one valitation at a time

--- src/CashFlow.Application/UseCases/Users/Register/RegisterUserValidator.cs
+++ src/CashFlow.Application/UseCases/Users/Register/RegisterUserValidator.cs
@@ -9,14 +9,16 @@ using System.Threading.Tasks;
 
 namespace CashFlow.Application.UseCases.Users.Register
 {
-    public class RegisterUserValidator: AbstractValidator<RequestRegisterUserJson>
+    public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
     {
         public RegisterUserValidator()
         {
             RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceErrorMessages.NAME_EMPTY);
             RuleFor(user => user.Email)
                 .NotEmpty().WithMessage(ResourceErrorMessages.EMAIL_EMPTY)
-                .EmailAddress().WithMessage(ResourceErrorMessages.EMAIL_INVALID);
+                .EmailAddress().WithMessage(ResourceErrorMessages.EMAIL_INVALID)
+                    .When(user => !string.IsNullOrWhiteSpace(user.Email), ApplyConditionTo.CurrentValidator)
+                ;
             RuleFor(user => user.Password).SetValidator(new PasswordValidator<RequestRegisterUserJson>());
         }
     }
