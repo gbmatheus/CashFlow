db764122fe3ee203a632796c28b2c32b1c1875ed
gbmatheus <gabrielmatheus2503@gmail.com>
2025-05-18 11:22:14 -0300

    test: change unit test register user use case and failure test

---
+++ tests/CommonTestUtilities/Tokens/AccessTokenGeneratorBuilder.cs
@@ -0,0 +1,17 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Security.Tokens;
+using Moq;
+
+namespace CommonTestUtilities.Tokens
+{
+    public static class AccessTokenGeneratorBuilder
+    {
+        public static IAccessTokenGenerator Build()
+        {
+            var mock = new Mock<IAccessTokenGenerator>();
+            mock.Setup(accessTokenGenerator => accessTokenGenerator.Generate(It.IsAny<User>()))
+                .Returns("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30");
+            return mock.Object;
+        }
+    }
+}
