a8ad97532b732911cdb382906f80b537c0155f15
gbmatheus <gabrielmatheus2503@gmail.com>
2025-05-18 11:22:14 -0300

    test: change unit test register user use case and failure test

--- tests/CommonTestUtilities/Repositories/UserReadRepositoryBuilder.cs
+++ tests/CommonTestUtilities/Repositories/UserReadRepositoryBuilder.cs
@@ -1,20 +1,20 @@
 ﻿using CashFlow.Domain.Repositories.Users;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CommonTestUtilities.Repositories
 {
     public class UserReadRepositoryBuilder
     {
-        new Mock<IUserReadOnlyRepository> _repository;
+        Mock<IUserReadOnlyRepository> _repository;
 
         public UserReadRepositoryBuilder()
         {
             _repository = new Mock<IUserReadOnlyRepository>();
+        }
+
+        public void ExistsActiveUserWithEmail(string email)
+        {
+            _repository.Setup(config => config.ExistsActiveUserWithEmail(email)).ReturnsAsync(true);
         }
 
         public IUserReadOnlyRepository Build()
--- tests/UseCase.Test/Users/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCase.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -1,14 +1,17 @@
 ﻿using AutoMapper;
 using CashFlow.Application.UseCases.Users.Register;
-using CashFlow.Domain.Repositories.Users;
 using CashFlow.Domain.Repositories;
+using CashFlow.Domain.Repositories.Users;
 using CashFlow.Domain.Security.Cryptography;
-using System.Security.Cryptography;
-using CommonTestUtilities.Request;
-using FluentAssertions;
+using CashFlow.Domain.Security.Tokens;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionBase;
+using CommonTestUtilities.Cryptography;
 using CommonTestUtilities.Mapper;
 using CommonTestUtilities.Repositories;
-using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Request;
+using CommonTestUtilities.Tokens;
+using FluentAssertions;
 
 namespace UseCase.Test.User.Register
 {
@@ -27,16 +30,45 @@ namespace UseCase.Test.User.Register
             result.Token.Should().NotBeNullOrWhiteSpace();
         }
 
-        private static RegisterUserUseCase CreateUseCase()
+        [Fact]
+        public async Task Error_Name_Empty()
+        {
+            var useCase = CreateUseCase();
+            var request = RequestRegisterUserJsonBuilder.Builder();
+            request.Name = string.Empty;
+
+            var act = async () => await useCase.Execute(request);
+
+            var result = await act.Should().ThrowAsync<ErroOnValidationException>();
+            result.Where(ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessages.NAME_EMPTY));
+        }
+
+        [Fact]
+        public async Task Error_Email_Already_Exists()
+        {
+            var request = RequestRegisterUserJsonBuilder.Builder();
+            var useCase = CreateUseCase(request.Email);
+            
+            var act = async () => await useCase.Execute(request);
+
+            var result = await act.Should().ThrowAsync<ErroOnValidationException>();
+            result.Where(ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessages.EMAIL_ALREADY_REGISTERED));
+        }
+
+        private static RegisterUserUseCase CreateUseCase(string email = null)
         {
             IMapper mapper = MapperBuilder.Builder();
             IUnitOfWork unitOfWork = UnitOfWorkBuilder.Build();
             IPasswordEncrypter passwordEncrypter = PasswordEncrypterBuilder.Build();
+            IAccessTokenGenerator accessToken = AccessTokenGeneratorBuilder.Build();
 
             var userReadOnlyRepository = new UserReadRepositoryBuilder();
+            if(!string.IsNullOrWhiteSpace(email))
+                userReadOnlyRepository.ExistsActiveUserWithEmail(email);
+
             IUserWriteOnlyRepository userWriteOnlyRepository = UserWriteRepositoryBuilder.Build();
             
-            return new RegisterUserUseCase(userReadOnlyRepository.Build(), userWriteOnlyRepository, mapper, passwordEncrypter, unitOfWork);
+            return new RegisterUserUseCase(userReadOnlyRepository.Build(), userWriteOnlyRepository, mapper, passwordEncrypter, unitOfWork, accessToken);
         }
     }
 }
